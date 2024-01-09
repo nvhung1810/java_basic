e3ef766d6e35c5e46420a41351211c64fa6f447e
nvhung1810 <nguyenvanhung181001@gmail.com>
2024-01-09 14:50:48 +0700

    feat: java operators

---
+++ src/components/data_types/DataTypes.java
@@ -0,0 +1,12 @@
+package components.data_types;
+
+public class DataTypes {
+  public static void main(String[] args) throws Exception {
+    /* KIEU DU LIEU */
+
+    // Nguyen thuy (Primitive data types): byte, short, int, long, float, double,
+    // boolean, char
+
+    // Phuc tap (Non-primitive data types): String, Arrays and Classes (sẽ học sau)
+  }
+}
