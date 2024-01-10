d5246a758f11f86d429e042536a95f0d38eccc6d
nvhung1810 <nguyenvanhung181001@gmail.com>
2024-01-10 09:27:11 +0700

    feat: object oriented programming

---
+++ src/components/object_oriented_programming/ObjectOrientedProgramming.java
@@ -0,0 +1,22 @@
+package components.object_oriented_programming;
+
+public class ObjectOrientedProgramming {
+  public static void main(String[] args) {
+    /*
+     * OOP có các lợi thế:
+     * - Clear Structure : cấu trúc rõ ràng
+     * - Reuse application code : tính tái sử dụng code cao => dễ dàng maintain (bảo
+     * trì),
+     * modify (sửa đổi) và debug (fix bug)
+     * - designing large and complex programs (tương tự angular) : mục tiêu giải
+     * quyết các bài toán phức tạp
+     */
+
+    /**
+     * OOP là cách chúng ta mô phỏng thế giới thực (real world) vào chương trình
+     * (program) máy tính.
+     * Tất cả các "đối tượng" tham gia vào chương trình chính là những thành phần
+     * chủ chốt.
+     */
+  }
+}
