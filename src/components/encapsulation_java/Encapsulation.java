f7cbfafe21d071d3109bee681facbc3af2436950
nvhung1810 <nguyenvanhung181001@gmail.com>
2024-01-10 15:24:32 +0700

    feat: encapsulation

---
+++ src/components/encapsulation_java/Encapsulation.java
@@ -0,0 +1,11 @@
+package components.encapsulation_java;
+
+public class Encapsulation {
+  // Tinh dong goi
+}
+/**
+ * han che quyen "modify" data
+ * che dau data: public/private
+ *
+ * phai dung geter, setter de lay du lieu
+ */
