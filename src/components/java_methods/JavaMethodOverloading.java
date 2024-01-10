340d8a5e7dff65d9ca07a5bf1a04f898b3a401de
nvhung1810 <nguyenvanhung181001@gmail.com>
2024-01-10 15:57:44 +0700

    feat: java methods

---
+++ src/components/java_methods/JavaMethodOverloading.java
@@ -0,0 +1,23 @@
+public class JavaMethodOverloading {
+  static int plusMethod(int x, int y) {
+    return x + y;
+  }
+
+  static double plusMethod(double x, double y) {
+    return x + y;
+  }
+
+  public static void main(String[] args) {
+    int myNum1 = plusMethod(8, 5);
+    double myNum2 = plusMethod(4.3, 6.26);
+    System.out.println("int: " + myNum1);
+    System.out.println("double: " + myNum2);
+  }
+}
+/**
+ * With method overloading, multiple methods can have the same name with
+ * different parameters:
+ *
+ * tum cai vay la: co the dinh nghia nhieu method co ten giong nhau, khac nhau
+ * doi so truyen vao la dc
+ */
