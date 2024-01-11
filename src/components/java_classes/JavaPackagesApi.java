1fc960ed6fbc700c7d0d760888f70ff56c6cabac
nvhung1810 <nguyenvanhung181001@gmail.com>
2024-01-11 08:51:49 +0700

    java packages and api

---
+++ src/components/java_classes/JavaPackagesApi.java
@@ -0,0 +1,17 @@
+package components.java_classes;
+
+public class JavaPackagesApi {
+  public static void main(String[] args) {
+    System.out.println("package is folder");
+  }
+}
+
+/**
+ * Java Packages & API
+ *
+ *
+ * A package in Java is used to group related classes.
+ *
+ * Built-in Packages (packages from the Java API)
+ * User-defined Packages (create your own packages)
+ */
